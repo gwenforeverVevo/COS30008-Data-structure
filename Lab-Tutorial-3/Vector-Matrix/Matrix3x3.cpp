9ca528563d62735dedad5c3a4b7d289b32c96558
Amani kamaruddin <126694143+gwenforeverVevo@users.noreply.github.com>
2024-09-12 19:11:18 +0800

    asds

--- Lab-Tutorial-3/Vector-Matrix/Matrix3x3.cpp
+++ Lab-Tutorial-3/Vector-Matrix/Matrix3x3.cpp
@@ -1,7 +1,5 @@
 #define _USE_MATH_DEFINES // must be defined before any #include
 #include "Matrix3x3.h"
-#include "Vector2D.h"
-#include "Vector3D.h"
 #include <cmath>
 #include <iostream>
 #include <fstream>
@@ -12,17 +10,17 @@ Matrix3x3::Matrix3x3() noexcept : fRows()
 {
 }
 
-Matrix3x3 &Matrix3x3::operator*(const float aScalar) const noexcept
+Matrix3x3 Matrix3x3::operator*(const float aScalar) const noexcept
 {
     return Matrix3x3(fRows[0] * aScalar, fRows[1] * aScalar, fRows[2] * aScalar);
 }
 
-Matrix3x3 &Matrix3x3::operator+(const Matrix3x3 &aVector) const noexcept
+Matrix3x3 Matrix3x3::operator+(const Matrix3x3 &aOther) const noexcept
 {
     return Matrix3x3(fRows[0] + aOther.fRows[0], fRows[1] + aOther.fRows[1], fRows[2] + aOther.fRows[2]);
 }
 
-Vector &Matrix3x3::operator*(const Vector3D &aVector) const noexcept
+Vector3D Matrix3x3::operator*(const Vector3D &aVector) const noexcept
 {
     return Vector3D(fRows[0].dot(aVector), fRows[1].dot(aVector), fRows[2].dot(aVector));
 }
