478378f289a6b119cdb88e325a28dcfc7251515a
Amani kamaruddin <126694143+gwenforeverVevo@users.noreply.github.com>
2024-09-19 15:47:40 +0800

    Add files via upload

---
+++ Lab-Tutorial-5/Tutorial-5/Map.cpp
@@ -0,0 +1,16 @@
+
+// COS30008, Tutorial 5, 2024
+
+#pragma once
+#include <Map.h>
+#include <istream>
+#include <ostream>
+
+Map::Map(){
+
+}
+
+istream $operator>>(istream& aIStream, Map& aMap)
+{
+    return string;
+}
