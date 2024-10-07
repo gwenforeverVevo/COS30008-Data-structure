794f4d12df33661416129f72f9bc6bdf21cc7f00
Amani kamaruddin <126694143+gwenforeverVevo@users.noreply.github.com>
2024-10-08 00:22:53 +0800

    yes

---
+++ Problem-Set-2/FibonacciSequenceGenerator.cpp
@@ -0,0 +1,53 @@
+#include "FibonacciSequenceGenerator.h"
+#include <cassert>
+#include <limits>
+#include <iostream>
+#include <string>
+using namespace std;
+
+FibonacciSequenceGenerator::FibonacciSequenceGenerator(const std::string &aID) noexcept
+    : fID(aID), fPrevious(0), fCurrent(1)
+{
+
+}
+const string &FibonacciSequenceGenerator::id() const noexcept
+{
+    return fID;
+}
+
+const long long &FibonacciSequenceGenerator::operator*() const
+{
+    return fCurrent;
+}
+
+FibonacciSequenceGenerator::operator bool() const noexcept
+{
+    return fCurrent > 0;
+}
+
+void FibonacciSequenceGenerator::reset() noexcept
+{
+    fPrevious = 0;
+    fCurrent = 1;
+}
+
+bool FibonacciSequenceGenerator::hasNext() const noexcept
+{   int64_t tempHasNext = numeric_limits<int64_t>::max();
+    // temp = fPrevious + fCurrent;
+    if (tempHasNext - fPrevious >= fCurrent) 
+    {
+        return true;
+    }
+    return false;
+}
+
+void FibonacciSequenceGenerator::next() noexcept
+{
+    int64_t temp;   
+    temp = fPrevious + fCurrent;
+    fPrevious = fCurrent;
+    fCurrent = temp;
+    assert(fCurrent > fPrevious);
+}
+
+
